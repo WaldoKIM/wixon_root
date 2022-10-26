bd5d84031b645d90878b4a3e44553ac552044478
JisungKim <intkim777@wixon.co.kr>
2022-10-26 13:32:43 +0900

    first commit

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get("/day", tags=["Dates"])
+def get_day_of_week():
+    """
+    Get the current day of week
+    """
+    return {
+        "day": datetime.now().strftime("%A")
+    }
+
+@app.get("/waldo")
+async def root():
+    return {"message": "Hello there!!!!"}    
---
+++ frontend/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        day: ""
+    };
+  }
+
+  componentDidMount() {
+      fetch('/api/day')
+          .then(response => response.json())
+          .then(response => this.setState({'day': response.day}))
+  }
+
+  render() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Hello There!!!</h1>
+        <p>윅슨 리액트 테스트</p>
+        <h3>Hey!  It's {this.state.day}</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>        
+      </header>
+    </div>
+    );
+  }
+}
+
+
+
+// function App() {
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <h1>Hello There!!!</h1>
+    //     <p>윅슨 리액트 테스트</p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>        
+    //   </header>
+    // </div>
+//   );  
+// }
+
+
+// export default App;
+
+export default App;
